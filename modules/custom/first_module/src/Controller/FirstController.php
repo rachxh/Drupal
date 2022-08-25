4cfd08cc29c9aa3e1419b103a325e18af2530def
Xiaohong Chen <s2200116@BC-C02GR0BGQ6LR-MacBook-Air.local>
2022-08-25 13:44:37 +0300

    added custom modules

---
+++ modules/custom/first_module/src/Controller/FirstController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\first_module\Controller;
+
+class FirstController {
+    
+    public function first(){
+
+        return array ('#markup'=>'Welcome to drupal website');
+
+    }
+}
---
+++ modules/custom/second_module/src/Controller/SecondController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\second_module\Controller;
+
+class SecondController {
+    
+    public function second(){
+
+        return array ('#markup'=>'Welcome to drupal website -Second Module');
+
+    }
+}
