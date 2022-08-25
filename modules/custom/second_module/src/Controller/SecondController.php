3e4586811c1df714d65e353561f07f2d861d6b50
Xiaohong Chen <s2200116@BC-C02GR0BGQ6LR-MacBook-Air.local>
2022-08-25 14:40:04 +0300

    second commit

--- modules/custom/second_module/src/Controller/SecondController.php
+++ modules/custom/second_module/src/Controller/SecondController.php
@@ -2,11 +2,23 @@
 
 namespace Drupal\second_module\Controller;
 
-class SecondController {
+// use Drupal\Core\Controller\ControllerBase;
+use \Drupal\Core\StringTranslation\StringTranslationTrait; //optional 
+
+// class SecondController extends ControllerBase{
     
+//     public function second(){
+
+//         // return array ('#markup'=>'Welcome to drupal website -Second Module');
+//         return ['#markup'=>$this->t('Welcome to Translation trait')];
+
+//     }
+// }
+
+class SecondController {
+    use StringTranslationTrait;
+
     public function second(){
-
-        return array ('#markup'=>'Welcome to drupal website -Second Module');
-
+        return ['#markup'=>$this->t('Welcome to Translation trait')];
     }
 }
